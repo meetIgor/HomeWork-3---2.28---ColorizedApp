567a5116fed90c0e85f2e81b0bd9fe9fe9c233db
meetIgor <iigoor.s@gmail.com>
2022-06-25 11:33:00 +0300

    version 1.0

--- HomeWork #3 - 2.28 - ColorizedApp/ViewController.swift
+++ HomeWork #3 - 2.28 - ColorizedApp/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //MARK: - IB Outlets
+    @IBOutlet weak var resultView: UIView!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet weak var redValueLabel: UILabel!
+    @IBOutlet weak var greenValueLabel: UILabel!
+    @IBOutlet weak var blueValueLabel: UILabel!
+    
+    //MARK: - Life Cycles Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        changeColor()
+        resultView.layer.cornerRadius = 30
     }
-
+    
+    //MARK: - IB Actions
+    @IBAction func slidersAction(_ sender: UISlider) {
+        changeColor()
+    }
+    
+    //MARK: - Private Methods
+    private func changeColor() {
+        resultView.backgroundColor = UIColor(
+            red: CGFloat( redSlider.value ),
+            green: CGFloat( greenSlider.value ),
+            blue: CGFloat( blueSlider.value ),
+            alpha: 1
+        )
+        
+        redValueLabel.text = String(format: "%.2f", redSlider.value)
+        greenValueLabel.text = String(format: "%.2f", greenSlider.value)
+        blueValueLabel.text = String(format: "%.2f", blueSlider.value)
+    }
 
 }
 
